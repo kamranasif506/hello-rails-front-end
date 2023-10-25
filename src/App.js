2b3fbb357162a1992d57bfaaf32f9b1b0a6d876e
Kamran Asif <98281453+kamranasif506@users.noreply.github.com>
2023-10-25 11:53:51 +0500

    issue resolved for react app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import { Route, Routes, NavLink } from 'react-router-dom';
+import { Route, Routes, Link } from 'react-router-dom';
 import Greeting from './components/Greeting';
 import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
-      <NavLink to="/greeting">
-        Greeting
-      </NavLink>
+      <div className="collapse navbar-collapse justify-content-start" id="navbarNav">
+        <ul className="navbar-nav">
+          <li className="nav-item border-right">
+            <Link className="nav-link" to="/">
+              Home
+            </Link>
+            </li>
+          <li>  
+            <Link className="nav-link" to="/greeting">
+              Greetings
+            </Link>
+          </li>
+        </ul>  
+      </div>  
+      
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/greeting" element={<Greeting />} />
